bc0c4ec0df684d7d6b005f57bf637f00172d3264
Hou <kchia87@gmail.com>
2014-10-22 13:57:43 -0700

    Implemented _.reject

--- src/underbar.js
+++ src/underbar.js
@@ -97,6 +97,13 @@ var _ = {};
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var rejected = [];
+    _.filter(collection, function(element){
+      if (!test(element)){
+        rejected.push(element);
+      }
+    });
+    return rejected;
   };
 
   // Produce a duplicate-free version of the array.
